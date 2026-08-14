7598bbc896668e58b59408a8834aeed036bccc37
nehanielrcm-afk <nehaniel.rcm@gmail.com>
2026-08-14 20:43:29 +0000

    Fix supabaseClient import depth

--- app/medicore/claims/page.js
+++ app/medicore/claims/page.js
@@ -3,8 +3,7 @@ export const dynamic = 'force-dynamic'
 
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
-import { getSupabaseClient } from '../../lib/supabaseClient'
-
+import { getSupabaseClient } from '../../../lib/supabaseClient'
 export default function ClaimsDashboard() {
   const [claims, setClaims] = useState([])
   const [loading, setLoading] = useState(true)
